d015e8d929b6d45725f1163d55de2b3499efdc32
Adam Boettcher <baboettcher@gmail.com>
2017-06-12 16:27:40 -0500

    test commit on work branch

--- src/monkeyDancer.js
+++ src/monkeyDancer.js
@@ -1,13 +1,13 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var monkeyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   makeDancer.call(this, top, left, timeBetweenSteps);
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+monkeyDancer.prototype = Object.create(makeDancer.prototype);
+monkeyDancer.prototype.constructor = monkeyDancer;
 
-makeBlinkyDancer.prototype.step = function() {
+monkeyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
