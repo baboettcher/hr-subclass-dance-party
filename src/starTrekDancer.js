d9a5215177079ac949d476848e7bd13717eeb12c
Adam Boettcher <baboettcher@gmail.com>
2017-06-12 15:57:37 -0500

    test branch commit

--- src/starTrekDancer.js
+++ src/starTrekDancer.js
@@ -1,6 +1,8 @@
 var makeStarTrekDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  // test branch
+
   makeDancer.call(this, top, left, timeBetweenSteps);
 
   var randomNumber = Math.floor(Math.random()*5);
